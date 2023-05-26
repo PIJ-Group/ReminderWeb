756ce2942a63ad1b46b250cf50b70cbddd3f47ce
Isra <pirravk@gmail.com>
2023-05-26 23:37:37 +0200

    Mejora de textos, optimización sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
 import { Menu } from './_model/menu';
 import { ServiceMenuService } from './service-menu.service';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +14,22 @@ export class AppComponent implements OnDestroy {
   title = 'Reminder';
   mobileQuery: MediaQueryList;
   menus: Menu[];
+  isSidenavOpen = false;
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private servicioMenu: ServiceMenuService) {
+  @ViewChild('snav') sidenav!: MatSidenav;
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private servicioMenu: ServiceMenuService
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-     this.menus = servicioMenu.menus;
+    this.menus = servicioMenu.menus;
+    this.isSidenavOpen = !this.mobileQuery.matches; // Abrir el sidenav en versión web y cerrarlo en versión móvil
   }
 
   ngOnDestroy(): void {
@@ -28,5 +37,15 @@ export class AppComponent implements OnDestroy {
   }
 
   shouldRun = true;
+
+  toggleSidenav(): void {
+    this.sidenav.toggle();
+  }
+
+  closeSidenav(): void {
+    if (this.mobileQuery.matches) {
+      this.sidenav.close();
+    }
+  }
 }
 
